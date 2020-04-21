611e2fc973bb82bb4c9e54f426865f4726eded08
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-04-21 04:48:09 -0300

    Módulo 2 - Aula 03 concluída

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\FormularioInsercao;
+
+switch($_SERVER['PATH_INFO']){
+    case '/listar-cursos':
+        $controlador = new ListarCursos();
+        $controlador->processaRequisicao();
+        break;
+
+    case '/novo-curso':
+        $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
+        break;
+    default:
+        echo "Erro 404 - Página não encontrada";
+}
+
+?>
